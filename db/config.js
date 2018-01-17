3bd01e0ffd06bd075a5f52446f10d4fd713f4894
Stephen-Harding <StephenHarding79@gmail.com>
2018-01-17 15:06:25 -0500

    changed db config

--- db/config.js
+++ db/config.js
@@ -1,23 +1,18 @@
-const options = {
-  query: (e) => {
-    console.log(e.query)
-  }
+const pgp = require('pg-promise')();
+
+let db;
+
+if (process.env.NODE_ENV === 'development' || !process.env.NODE_ENV) {
+  db = pgp({
+  // custom database name
+  database: 'subway_db', 
+  port: 5432,
+  host: 'localhost',
+  });
+} else if (process.env.NODE_ENV === 'production') {
+  // Heroku will add this
+  db = pgp(process.env.DATABASE_URL); 
 }
-
-const pgp = require('pg-promise')(options)
-
-function setDatabase(){
-  if (process.env.NODE_ENV === 'development' || !process.env.NODE_ENV) {
-    return pgp({
-      database: 'subway_db',
-      port: 5432,
-      host: 'localhost'
-    })
-  } else if (process.env.NODE_ENV === 'production'){
-    return pgp(process.env.DATABASE_URL)
-  }
-}
-
-const db = setDatabase();
 
 module.exports = db;
+
